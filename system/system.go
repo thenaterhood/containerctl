1a18bfa985df85e8f014c1971c759c9de513f068
Nate Levesque <public@thenaterhood.com>
2016-02-22 19:23:11 -0500

    Add user/group functions

---
+++ system/system.go
@@ -0,0 +1,89 @@
+package system
+
+import(
+    "os"
+    "io"
+    "encoding/csv"
+    "log"
+    "strconv"
+)
+
+type PasswdEntry struct {
+    Name string
+    Uid int
+    Gid int
+    Shell string
+    Fullname string
+    Home string
+}
+
+type SystemUser struct {
+    Name string
+    Uid int
+    Gid int
+    Shell string
+    Fullname string
+    Home string
+    Pwhash string
+}
+
+type SystemGroup struct {
+    Name string
+    Id int
+}
+
+
+func ReadPasswdEntry(r []string) (p *PasswdEntry) {
+    p = new(PasswdEntry)
+
+    p.Name = r[0]
+    p.Uid, _ = strconv.Atoi(r[2])
+    p.Gid, _ = strconv.Atoi(r[3])
+    p.Fullname = r[4]
+    p.Home = r[5]
+    p.Shell = r[6]
+
+    return p
+}
+
+func ReadEtcPasswd(path string) []*PasswdEntry {
+    var entries []*PasswdEntry
+
+    f, err := os.Open(path)
+
+    if err != nil {
+        return entries
+    }
+
+    csvreader := csv.NewReader(f)
+    csvreader.Comma = ':'
+
+    for {
+        record, err := csvreader.Read()
+        if err != nil {
+            if err == io.EOF {
+                break
+            }
+            log.Fatal(err)
+        }
+
+        entries = append(entries, ReadPasswdEntry(record))
+    }
+
+    return entries
+}
+
+func (c Container) AddHostUser(u system.SystemUser) {
+    etcShadowPath := path.Join(c.Location, c.Name, "etc", "shadow")
+    etcPasswdPath := path.Join(c.Location, c.Name, "etc", "passwd")
+    //etcGroupsPath := path.Join(c.Location, c.Name, "etc", "groups")
+
+    uids := strconv.Itoa(u.Uid)
+    gids := strconv.Itoa(u.Gid)
+
+    shadow, _ := os.OpenFile(etcShadowPath, os.O_APPEND|os.O_WRONLY, 0600)
+    shadow.WriteString(u.Name + ":" + u.Pwhash + ":" + "14675" + ":" + "99999" + ":" + "99999" + ":" + "7" + ":::")
+
+    passwd, _ := os.OpenFile(etcPasswdPath, os.O_APPEND|os.O_WRONLY, 0600)
+    passwd.WriteString(u.Name + ":x:" + uids + ":" + gids + "::" + "/home/" + u.Name + ":" + u.Shell)
+}
