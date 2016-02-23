00fd061d0adb7b9d63f98bcbd2dca3bd1c8b7173
Nate Levesque <public@thenaterhood.com>
2016-02-23 09:44:04 -0500

    Add ability to read etc shadow and passwd

--- system/system.go
+++ system/system.go
@@ -17,19 +17,61 @@ type PasswdEntry struct {
     Home string
 }
 
-type SystemUser struct {
-    Name string
-    Uid int
-    Gid int
-    Shell string
-    Fullname string
-    Home string
-    Pwhash string
+type OSUser struct {
+    ShadowEntry []string
+    PasswdEntry []string
 }
 
-type SystemGroup struct {
-    Name string
-    Id int
+type OSGroup struct {
+    GroupEntry []string
+}
+
+func LoadUsers(root string) []*OSUser {
+  var users []*OSUser
+
+  shadowf, serr := os.Open(path.Join(root, "etc", "shadow"))
+  passwdf, perr := os.Open(path.Join(root, "etc", "passwd"))
+
+  var pwentries, shadowentries [][]string
+
+  if serr != nil || perr != nil {
+    return users
+  }
+
+  shadowreader := csv.NewReader(shadowf)
+  shadowreader.Comma = ':'
+
+  for {
+    record, err := shadowreader.Read()
+    if err != nil {
+      if err == io.EOF {
+        break
+      }
+      log.Fatal(err)
+    }
+
+    append(shadowentries, record)
+  }
+
+  passwdreader := csv.NewReader(passwdf)
+  passwdreader.Comma = ':'
+
+  for {
+    record, err := passwdreader.Read()
+    if err != nil {
+      if err == io.EOF {
+        break
+      }
+      log.Fatal(err)
+    }
+
+    for shadowentry := range shadowentries {
+      if shadowentry[0] == record[0] {
+        append(users, OSUser{shadowentry, entry})
+        break
+      }
+    }
+  }
 }
 
 
